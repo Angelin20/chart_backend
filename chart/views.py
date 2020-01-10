febdf48cc5b939781c70ddd793d11b052841932e
Angelin20 <angel_javierlb@hotmail.com>
2020-01-09 19:07:23 -0600

    WIP: Add autentification without configurations for front credentials

--- chart/views.py
+++ chart/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from rest_framework import viewsets
+#from rest_framework.authentication import TokenAuthentication
+#from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from . import models
@@ -8,6 +10,8 @@ from . import serializers
 class ChartList(generics.ListCreateAPIView):
     queryset = models.Chart.objects.all().order_by('-created_at', '-updated_at')
     serializer_class = serializers.ChartSerializer
+    #authentication_classes = [TokenAuthentication, ]
+    #permission_classes = [IsAuthenticated, ]
 
 class ChartDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Chart.objects.all()
